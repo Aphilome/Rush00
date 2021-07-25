c2f985d127d090c5d147854e26c617752c176e85
Aphilome <1.katrin.1@mail.ru>
2021-07-25 19:11:42 +0300

    guid

--- rush00.Data/Models/Habit.cs
+++ rush00.Data/Models/Habit.cs
@@ -1,11 +1,17 @@
-﻿
+﻿using System;
+
 namespace rush00.Data.Models
 {
     public class Habit
     {
+        public Guid Id { get; set; }
+        
         public string Title { get; set; }
+        
         public string Motivation { get; set; }
-        public HabitCheck[]? Checks { get; set; }
+        
         public bool IsFinished { get; set; }
+        
+        public HabitCheck[] Checks { get; set; }
     }
 }
--- rush00.Data/Models/HabitCheck.cs
+++ rush00.Data/Models/HabitCheck.cs
@@ -4,6 +4,8 @@ namespace rush00.Data.Models
 {
     public class HabitCheck
     {
+        public Guid Id { get; set; }
+        
         public DateTime Date { get; set; }
         
         public bool IsChecked { get; set; }
