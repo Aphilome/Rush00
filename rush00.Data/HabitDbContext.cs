9730dcb0fee884c7a0e87dfb1e974bce478a07d6
Aphilome <1.katrin.1@mail.ru>
2021-07-25 20:56:13 +0300

    Migrations

--- rush00.Data/HabitDbContext.cs
+++ rush00.Data/HabitDbContext.cs
@@ -5,8 +5,8 @@ namespace rush00.Data
 {
     public class HabitDbContext: DbContext
     {
-        public DbSet<Habit> Habits;
-        public DbSet<HabitCheck> HabitChecks;
+        public DbSet<Habit> Habits { get; set; }
+        public DbSet<HabitCheck> HabitChecks { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
---
+++ rush00.Data/Migrations/20210725175419_InitDb.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace rush00.Data.Migrations
+{
+    public partial class InitDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Habits",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", nullable: true),
+                    Motivation = table.Column<string>(type: "TEXT", nullable: true),
+                    IsFinished = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Habits", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HabitChecks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    IsChecked = table.Column<bool>(type: "INTEGER", nullable: false),
+                    HabitId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HabitChecks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_HabitChecks_Habits_HabitId",
+                        column: x => x.HabitId,
+                        principalTable: "Habits",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HabitChecks_HabitId",
+                table: "HabitChecks",
+                column: "HabitId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HabitChecks");
+
+            migrationBuilder.DropTable(
+                name: "Habits");
+        }
+    }
+}
